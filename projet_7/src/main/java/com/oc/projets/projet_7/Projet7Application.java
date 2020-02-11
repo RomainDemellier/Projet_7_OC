6b66e91b412260a615adf6ca4d74cb59c7800b7b
romain <romaindemellier@yahoo.fr>
2020-02-11 15:25:21 +0100

    Création du projet 7

---
+++ projet_7/src/main/java/com/oc/projets/projet_7/Projet7Application.java
@@ -0,0 +1,13 @@
+package com.oc.projets.projet_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet7Application.class, args);
+	}
+
+}
---
+++ projet_7/src/test/java/com/oc/projets/projet_7/Projet7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oc.projets.projet_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
